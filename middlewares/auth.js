95977ed37125d4d48b7776421398874fff9281aa
Jiangning Huang <hjnyll@163.com>
2015-05-16 17:51:23 +0800

    1.9

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -113,6 +113,7 @@ exports.authWeixin = function (req, res, next) {
         var code = req.query.code;
         if (code) {
           console.log(code);
+          console.log('https://api.weixin.qq.com/sns/oauth2/access_token?appid=' + appid + '&secret=' + secret + '&code=' + code + '&grant_type=authorization_code');
           superagent
           .get('https://api.weixin.qq.com/sns/oauth2/access_token')
           .query({appid: appid, secret: secret, code: code, grant_type: 'authorization_code'})
